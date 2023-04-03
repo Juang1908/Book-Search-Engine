6680e14c451444aaaecc3b630c1c59ceba70925b
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2023-04-03 16:50:24 -0500

    .gitignore

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,13 +1,11 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { LOGIN_USER } from "../utils/mutations";
-// import { loginUser } from '../utils/API';
+
+import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { loginMutation } from "@apollo/client";
 
 const LoginForm = () => {
-  const [login] = loginMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -28,13 +26,15 @@ const LoginForm = () => {
     }
 
     try {
-      const {data} = await login({variables:{...userFormData}});
+      const response = await loginUser(userFormData);
 
-      if (!data) {
+      if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      Auth.login(data.login.token);
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
